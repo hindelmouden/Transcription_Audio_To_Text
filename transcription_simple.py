0b52467c98de2b44b45b1e6a936cd5bd4251f382
HIND EL MOUDEN <65297319+hindelmouden@users.noreply.github.com>
2025-05-18 16:34:04 +0100

    Create transcription_simple.py

---
+++ transcription_simple.py
@@ -0,0 +1,11 @@
+import os
+import whisper
+
+os.environ["PATH"] += os.pathsep + r"C:\Users\PC\Documents\s2-master\projet3-ml\ffmpeg\ffmpeg-7.1.1-full_build\bin"  # modifie ici selon ton installation
+
+model = whisper.load_model("base")
+
+audio_path = r"C:\Users\PC\AppData\Local\Temp\gradio\d32914a9d3de67e6fe96a18d42770a06a0aee34735345f6afde825457e5a9a97\ton_audio.wav"
+
+result = model.transcribe(audio_path)
+print(result["text"])
