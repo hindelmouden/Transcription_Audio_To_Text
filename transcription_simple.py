7dd15dcd86f1f48cd1531d8e7859324bc0333387
HIND EL MOUDEN <65297319+hindelmouden@users.noreply.github.com>
2025-05-19 23:37:45 +0100

    Update transcription_simple.py

--- transcription_simple.py
+++ transcription_simple.py
@@ -5,7 +5,7 @@ os.environ["PATH"] += os.pathsep + r"C:\Users\PC\Documents\s2-master\projet3-ml\
 
 model = whisper.load_model("base")
 
-audio_path = r"C:\Users\PC\AppData\Local\Temp\gradio\d32914a9d3de67e6fe96a18d42770a06a0aee34735345f6afde825457e5a9a97\ton_audio.wav"
+audio_path = r"C:\Users\PC\AppData\Local\Temp\gradio\d32914a9d3de67e6fe96a18d42770a06a0aee34735345f6afde825457e5a9a97\data\exemple_audio_fr.mp3"
 
 result = model.transcribe(audio_path)
 print(result["text"])
